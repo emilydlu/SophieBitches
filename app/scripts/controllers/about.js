32026118f541b1d09c39a52b60394c65d53a45d8
Emily Lu <emily@Emilys-MacBook-Air.local>
2016-07-04 14:49:07 -0400

    yeoman generated boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sophieBitchesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sophieBitchesApp
+ */
+angular.module('sophieBitchesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sophieBitchesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sophieBitchesApp
+ */
+angular.module('sophieBitchesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
