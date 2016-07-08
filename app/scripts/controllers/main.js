849091f7c96cf7f09187e8cdcbbc7c9e0a270b7b
Emily Lu <luem@bc.edu>
2016-07-07 23:18:57 -0400

    adding functionality any and voice recording

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,70 @@
  * # MainCtrl
  * Controller of the sophieBitchesApp
  */
+
 angular.module('sophieBitchesApp')
-  .controller('MainCtrl', function () {
+.controller('MainCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var vm = this;
+
+   this.fuckingDelight = function() {
+   	var formation = document.getElementById("fuckingDelight");
+   	if (formation.paused == true){
+   		formation.play();
+   	} else {
+   		formation.pause();
+   	}
+  };
+
+  this.icedChai = function(){
+  var icedChai = document.getElementById("icedChai");
+   	if (icedChai.paused == true){
+   		icedChai.play();
+   	} else {
+   		icedChai.pause();
+   	}
+  };
+
+  this.dasSoMean = function(){
+  var dasSomean = document.getElementById("dasSomean");
+   	if (dasSomean.paused == true){
+   		dasSomean.play();
+   	} else {
+   		dasSomean.pause();
+   	}
+  };
+   
+   this.tempurpedic = function(){
+  var tempurpedic = document.getElementById("tempurpedic");
+   	if (tempurpedic.paused == true){
+   		tempurpedic.play();
+   	} else {
+   		tempurpedic.pause();
+   	}
+  };
+   
+   this.parks = function(){
+  var parks = document.getElementById("parks");
+   	if (parks.paused == true){
+   		parks.play();
+   	} else {
+   		parks.pause();
+   	}
+  };
+   
+   this.canUNot = function(){
+  var canUNot = document.getElementById("canUNot");
+   	if (canUNot.paused == true){
+   		canUNot.play();
+   	} else {
+   		canUNot.pause();
+   	}
+  };
   });
+
+
